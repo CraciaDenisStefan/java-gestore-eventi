0d07edd787c3c5fafe8544e85bb62e4b30faac86
CraciDenisStefan <crdenis13@gmail.com>
2023-11-21 10:40:57 +0100

    milestone 1 ok

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,5 +1,41 @@
 package org.lessons.java;
 
+import java.time.LocalDate;
+import org.lessons.java.pojo.abs.Evento;
 public class Main {
+	public static void main(String[] args) {
+        try {
+          
+            Evento evento1 = new Evento("Evento 1", LocalDate.now().plusDays(10), 50);
+            Evento evento2 = new Evento("Evento 2", LocalDate.now().plusDays(20), 100);
 
+       
+            Evento[] eventi = {evento1, evento2};
+
+            
+            for (Evento evento : eventi) {
+                System.out.println(evento);
+            }
+
+   
+            evento1.prenota();
+            evento2.prenota();
+            evento1.disdici();
+            evento2.prenota();
+            evento2.prenota();
+            evento2.disdici();
+
+   
+            for (Evento evento : eventi) {
+                System.out.println(evento);
+                System.out.println("Posti prenotati: " + evento.getPostiPrenotati());
+                System.out.println("Posti disponibili: " + (evento.getPostiTotali() - evento.getPostiPrenotati()));
+                System.out.println();
+            }
+
+        } catch (Exception e) {
+       
+            System.out.println("Errore: " + e.getMessage());
+        }
+    }
 }
--- src/org/lessons/java/pojo/abs/Evento.java
+++ src/org/lessons/java/pojo/abs/Evento.java
@@ -1,5 +1,74 @@
 package org.lessons.java.pojo.abs;
 
-public class Evento {
+import java.time.LocalDate;
 
+public  class Evento {
+    private String titolo;
+    private LocalDate data;
+    private int postiTotali;
+    private int postiPrenotati;
+
+    public Evento(String titolo, LocalDate data, int postiTotali) throws Exception{
+        setTitolo(titolo);
+        setData(data);
+        setPostiTotali(postiTotali);
+        setPostiPrenotati(0);
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) throws Exception {
+        if (data.isBefore(LocalDate.now())) {
+            throw new Exception("La data è già passata.");
+        }
+        this.data = data;
+    }
+
+    public int getPostiTotali() {
+        return postiTotali;
+    }
+
+    private void setPostiTotali(int postiTotali) throws Exception {
+        if (postiTotali <= 0) {
+            throw new Exception("Il numero di posti totali deve essere positivo.");
+        }
+        this.postiTotali = postiTotali;
+    }
+    
+    public int getPostiPrenotati() {
+        return postiPrenotati;
+    }
+
+    private void setPostiPrenotati(int postiPrenotati) {
+        this.postiPrenotati = postiPrenotati;
+    }
+
+    public void prenota() throws Exception {
+        if (data.isBefore(LocalDate.now()) || postiPrenotati >= postiTotali) {
+            throw new Exception("Impossibile effettuare la prenotazione.");
+        }
+        setPostiPrenotati(getPostiPrenotati() + 1);
+    }
+
+    public void disdici() throws Exception {
+        if (data.isBefore(LocalDate.now()) || postiPrenotati == 0) {
+            throw new Exception("Impossibile effettuare la disdetta.");
+        }
+        setPostiPrenotati(getPostiPrenotati() - 1);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - %s", getData(), getTitolo());
+    }
 }
